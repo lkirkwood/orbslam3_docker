14be62e68f4bee6a7db41ace0d5cfd41ab676ac1
Linus Kirkwood <linuskirkwood@gmail.com>
2025-05-09 22:24:10 +1000

    Add rotation interpolation

--- render-replica-frames.py
+++ render-replica-frames.py
@@ -4,9 +4,11 @@ import habitat_sim
 import habitat_sim.agent
 import habitat_sim.utils.common as utils
 import numpy as np
+import quaternion
 import imageio
 import os
 import shutil
+from math import ceil
 
 scene_config_file = "/Datasets/Replica/scene_dataset_config.json"
 output_dir = "/Datasets/habsim-Replica"
@@ -22,14 +24,15 @@ def make_cfg() -> habitat_sim.Configuration:
     sim_cfg.gpu_device_id = -1
 
     rgb_sensor = habitat_sim.CameraSensorSpec()
+    rgb_sensor.uuid = "rgb_sensor"
     rgb_sensor.sensor_type = habitat_sim.SensorType.COLOR
-    rgb_sensor.resolution = [640, 480]
+    rgb_sensor.resolution = [1280, 960]
     rgb_sensor.position = [0.0, 1.5, 0.0]
 
     depth_sensor = habitat_sim.CameraSensorSpec()
-    rgb_sensor.uuid = "depth_sensor"
-    rgb_sensor.sensor_type = habitat_sim.SensorType.DEPTH
-    depth_sensor.resolution = [640, 480]
+    depth_sensor.uuid = "depth_sensor"
+    depth_sensor.sensor_type = habitat_sim.SensorType.DEPTH
+    depth_sensor.resolution = [1280, 960]
     depth_sensor.position = [0.0, 1.5, 0.0]
 
     agent_cfg = habitat_sim.agent.AgentConfiguration()
@@ -44,7 +47,7 @@ pathfinder = sim.pathfinder
 assert pathfinder.is_loaded, (
     f"Navmesh not loaded - check the config at {scene_config_file}"
 )
-seed = 123456789
+seed = 424242
 pathfinder.seed(seed)
 
 start = pathfinder.get_random_navigable_point()
@@ -74,31 +77,22 @@ for i in range(len(path.points) - 1):
         continue
     direction = segment / dist
     for j in range(num_steps):
-        interpolated.append((p0 + j * step_size * direction).tolist())
+        interpolated.append(p0 + j * step_size * direction)
 interpolated.append(path.points[-1])
+ty = interpolated[0][1]
 
-
+# Walk path and observe #########################################################
 rgb_timestamps = []
 depth_timestamps = []
 positions = []
-for i, pos in enumerate(interpolated):
-    agent = sim.get_agent(0)
-    state = habitat_sim.AgentState()
-    state.position = pos
 
-    if i + 1 < len(interpolated):
-        forward = np.array(interpolated[i + 1]) - np.array(pos)
-        forward[1] = 0  # flatten to straight ahead
-        forward = forward / np.linalg.norm(forward)
-        rotation = utils.quat_from_two_vectors(np.array([0, 0, -1]), forward)
-        state.rotation = rotation
-    agent.set_state(state)
 
+def observe(frame: int):
     obs = sim.get_sensor_observations()
 
-    cur_time = i / 30.0
+    cur_time = frame / 30.0
     timestamp = f"{cur_time:03f}"
-    rgb_frame = obs["rgba_camera"]
+    rgb_frame = obs["rgb_sensor"]
     rgb_frame_path = f"rgb/frame_{timestamp}.png"
     imageio.imwrite(f"{output_dir}/{rgb_frame_path}", rgb_frame)
 
@@ -111,15 +105,71 @@ for i, pos in enumerate(interpolated):
         f"{timestamp} {rgb_frame_path} {timestamp} {depth_frame_path}\n"
     )
 
-    # positions
-    tx, ty, tz = pos
+    tx, _, tz = pos
     qx, qy, qz, qw = (
         state.rotation.x,
         state.rotation.y,
         state.rotation.z,
         state.rotation.w,
     )
-    positions.append(f"{timestamp} {tx} {ty} {tz} {qx} {qy} {qz} {qw}\n")
+    positions.append(f"{i:03d} {tx} {ty} {tz} {qx} {qy} {qz} {qw}\n")
+
+
+agent = sim.get_agent(0)
+prev_rotation = None
+prev_pos = None
+rot_threshold = 0.02
+extra_steps = 0
+for i, pos in enumerate(interpolated):
+    pos = pos.copy()
+    pos[1] = ty
+    state = habitat_sim.AgentState()
+    frame = i + extra_steps
+
+    if i + 1 < len(interpolated):
+        forward = np.array(interpolated[i + 1]) - np.array(pos)
+        forward[1] = 0  # flatten to straight ahead
+        forward = forward / np.linalg.norm(forward)
+        rotation = utils.quat_from_two_vectors(np.array([0, 0, -1]), forward)
+
+        if prev_rotation:
+            rot_diff = np.quaternion(
+                rotation.w - prev_rotation.w, 0, rotation.y - prev_rotation.y, 0
+            )
+        else:
+            rot_diff = np.quaternion(0, 0, 0, 0)
+
+        # Sharp turn, interpolate further
+        if (abs(rot_diff.w) + abs(rot_diff.y)) > rot_threshold:
+            print("=" * 80)
+            print(f"Rotated sharply: {prev_rotation} -> {rotation}")
+            print("=" * 80)
+            rot_steps = int(max(abs(rot_diff.w), abs(rot_diff.y)) / rot_threshold)
+            rot_step_delta = rot_diff / rot_steps
+            pos_step_delta = (pos - prev_pos) / rot_steps
+            extra_steps += rot_steps + 1
+            for step_num in range(rot_steps + 1):
+                # interpolate
+                interp_rot = quaternion.slerp_evaluate(
+                    prev_rotation, rotation, step_num / rot_steps
+                )
+                # move
+                state.position = pos + (pos_step_delta * step_num)
+                state.rotation = interp_rot
+                # observe
+                agent.set_state(state)
+                frame += 1
+                observe(frame)
+
+        # No sharp turn, just move and observe
+        else:
+            state.position = pos
+            state.rotation = rotation
+            agent.set_state(state)
+            observe(frame)
+
+        prev_rotation = rotation
+        prev_pos = pos
 
 with open(f"{output_dir}/rgb-timestamps", "w") as stream:
     stream.writelines(rgb_timestamps)
@@ -130,5 +180,7 @@ with open(f"{output_dir}/depth-timestamps", "w") as stream:
 with open(f"{output_dir}/positions", "w") as stream:
     stream.writelines(positions)
 
-print(f"Rendered {len(interpolated)} frames from {start} to {goal}, with seed {seed}")
+print(
+    f"Rendered {len(interpolated) + extra_steps} frames from {start} to {goal}, with seed {seed}"
+)
 sim.close()
