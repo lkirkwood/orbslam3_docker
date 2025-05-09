7e63d1e79d4f5e22ce746422d3b04bb6d81a58f2
Linus Kirkwood <linuskirkwood@gmail.com>
2025-05-09 11:56:25 +1000

    Add depth sensor, fix framerate and add trajectory

--- render-replica-frames.py
+++ render-replica-frames.py
@@ -6,10 +6,14 @@ import habitat_sim.utils.common as utils
 import numpy as np
 import imageio
 import os
+import shutil
 
 scene_config_file = "/Datasets/Replica/scene_dataset_config.json"
 output_dir = "/Datasets/habsim-Replica"
+shutil.rmtree(output_dir + "/rgb")
 os.makedirs(output_dir + "/rgb", exist_ok=True)
+shutil.rmtree(output_dir + "/depth")
+os.makedirs(output_dir + "/depth", exist_ok=True)
 
 
 def make_cfg() -> habitat_sim.Configuration:
@@ -17,16 +21,19 @@ def make_cfg() -> habitat_sim.Configuration:
     sim_cfg.scene_dataset_config_file = scene_config_file
     sim_cfg.gpu_device_id = -1
 
-    sensor_specs = []
-    sensor_spec = habitat_sim.CameraSensorSpec()
-    sensor_spec.uuid = "color_sensor"
-    sensor_spec.sensor_type = habitat_sim.SensorType.COLOR
-    sensor_spec.resolution = [512, 384]
-    sensor_spec.position = [0.0, 1.5, 0.0]
-    sensor_specs.append(sensor_spec)
+    rgb_sensor = habitat_sim.CameraSensorSpec()
+    rgb_sensor.sensor_type = habitat_sim.SensorType.COLOR
+    rgb_sensor.resolution = [640, 480]
+    rgb_sensor.position = [0.0, 1.5, 0.0]
+
+    depth_sensor = habitat_sim.CameraSensorSpec()
+    rgb_sensor.uuid = "depth_sensor"
+    rgb_sensor.sensor_type = habitat_sim.SensorType.DEPTH
+    depth_sensor.resolution = [640, 480]
+    depth_sensor.position = [0.0, 1.5, 0.0]
 
     agent_cfg = habitat_sim.agent.AgentConfiguration()
-    agent_cfg.sensor_specifications = sensor_specs
+    agent_cfg.sensor_specifications = [rgb_sensor, depth_sensor]
 
     return habitat_sim.Configuration(sim_cfg, [agent_cfg])
 
@@ -54,7 +61,7 @@ if not found:
     exit(1)
 
 
-step_size = 0.01
+step_size = 0.025
 interpolated = []
 for i in range(len(path.points) - 1):
     p0 = np.array(path.points[i])
@@ -71,7 +78,9 @@ for i in range(len(path.points) - 1):
 interpolated.append(path.points[-1])
 
 
-timestamps = []
+rgb_timestamps = []
+depth_timestamps = []
+positions = []
 for i, pos in enumerate(interpolated):
     agent = sim.get_agent(0)
     state = habitat_sim.AgentState()
@@ -79,19 +88,47 @@ for i, pos in enumerate(interpolated):
 
     if i + 1 < len(interpolated):
         forward = np.array(interpolated[i + 1]) - np.array(pos)
-        forward[1] = 0  # flatten to avoid tilting camera
+        forward[1] = 0  # flatten to straight ahead
         forward = forward / np.linalg.norm(forward)
         rotation = utils.quat_from_two_vectors(np.array([0, 0, -1]), forward)
         state.rotation = rotation
     agent.set_state(state)
 
     obs = sim.get_sensor_observations()
-    frame = obs["color_sensor"]
-    imageio.imwrite(f"{output_dir}/rgb/frame_{i:03d}.png", frame)
-    timestamps.append(f"{i:03d}\n")
 
-with open(f"{output_dir}/timestamps", "w") as stream:
-    stream.writelines(timestamps)
+    cur_time = i / 30.0
+    timestamp = f"{cur_time:03f}"
+    rgb_frame = obs["rgba_camera"]
+    rgb_frame_path = f"rgb/frame_{timestamp}.png"
+    imageio.imwrite(f"{output_dir}/{rgb_frame_path}", rgb_frame)
+
+    depth_frame = obs["depth_sensor"]
+    depth_frame_path = f"depth/frame_{timestamp}.exr"
+    imageio.imwrite(f"{output_dir}/{depth_frame_path}", depth_frame)
+
+    rgb_timestamps.append(f"{timestamp}\n")
+    depth_timestamps.append(
+        f"{timestamp} {rgb_frame_path} {timestamp} {depth_frame_path}\n"
+    )
+
+    # positions
+    tx, ty, tz = pos
+    qx, qy, qz, qw = (
+        state.rotation.x,
+        state.rotation.y,
+        state.rotation.z,
+        state.rotation.w,
+    )
+    positions.append(f"{timestamp} {tx} {ty} {tz} {qx} {qy} {qz} {qw}\n")
+
+with open(f"{output_dir}/rgb-timestamps", "w") as stream:
+    stream.writelines(rgb_timestamps)
+
+with open(f"{output_dir}/depth-timestamps", "w") as stream:
+    stream.writelines(depth_timestamps)
+
+with open(f"{output_dir}/positions", "w") as stream:
+    stream.writelines(positions)
 
 print(f"Rendered {len(interpolated)} frames from {start} to {goal}, with seed {seed}")
 sim.close()
