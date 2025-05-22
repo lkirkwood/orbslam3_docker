8a840d1ee2b96fd769fb01b5ed732ec306def743
Linus Kirkwood <linuskirkwood@gmail.com>
2025-05-22 17:33:04 +1000

    Render script now takes a series of points

--- render-replica-frames.py
+++ render-replica-frames.py
@@ -40,6 +40,35 @@ def make_cfg() -> habitat_sim.Configuration:
     return habitat_sim.Configuration(sim_cfg, [agent_cfg])
 
 
+def interpolate_path(start, goal) -> list:
+    path = habitat_sim.ShortestPath()
+    path.requested_start = start
+    path.requested_end = goal
+    found = pathfinder.find_path(path)
+
+    if not found:
+        print("No path found.")
+        sim.close()
+        exit(1)
+
+    step_size = 0.025
+    interpolated = []
+    for i in range(len(path.points) - 1):
+        p0 = np.array(path.points[i])
+        p1 = np.array(path.points[i + 1])
+        segment = p1 - p0
+        dist = np.linalg.norm(segment)
+        num_steps = int(np.floor(dist / step_size))
+        if num_steps == 0:
+            interpolated.append(p0.tolist())
+            continue
+        direction = segment / dist
+        for j in range(num_steps):
+            interpolated.append(p0 + j * step_size * direction)
+    interpolated.append(path.points[-1])
+    return interpolated
+
+
 sim = habitat_sim.Simulator(make_cfg())
 
 pathfinder = sim.pathfinder
@@ -49,36 +78,20 @@ assert pathfinder.is_loaded, (
 seed = 424242
 pathfinder.seed(seed)
 
-start = pathfinder.get_random_navigable_point()
-goal = pathfinder.get_random_navigable_point()
+waypoints = [
+    [2.7400379180908203, -1.600136637687683, -3.5185205936431885],
+    [6.584749698638916, -1.600136637687683, -3.7201988697052],
+    [6.034043312072754, -1.600136637687683, -5.86156702041626],
+    [2.6886186599731445, -1.600136637687683, -5.4488091468811035],
+]
 
-path = habitat_sim.ShortestPath()
-path.requested_start = start
-path.requested_end = goal
-found = pathfinder.find_path(path)
+path_points = []
+for wp_num in range(len(waypoints)):
+    start = waypoints[wp_num]
+    goal = waypoints[wp_num + 1] if (wp_num + 1) < len(waypoints) else waypoints[0]
+    path_points.extend(interpolate_path(start, goal))
 
-if not found:
-    print("No path found.")
-    sim.close()
-    exit(1)
-
-
-step_size = 0.025
-interpolated = []
-for i in range(len(path.points) - 1):
-    p0 = np.array(path.points[i])
-    p1 = np.array(path.points[i + 1])
-    segment = p1 - p0
-    dist = np.linalg.norm(segment)
-    num_steps = int(np.floor(dist / step_size))
-    if num_steps == 0:
-        interpolated.append(p0.tolist())
-        continue
-    direction = segment / dist
-    for j in range(num_steps):
-        interpolated.append(p0 + j * step_size * direction)
-interpolated.append(path.points[-1])
-ty = interpolated[0][1]
+ty = -1.6
 
 # Walk path and observe #########################################################
 rgb_timestamps = []
@@ -119,17 +132,24 @@ prev_rotation = None
 prev_pos = None
 rot_threshold = 0.02
 extra_steps = 0
-for i, pos in enumerate(interpolated):
+for i, pos in enumerate(path_points):
     pos = pos.copy()
     pos[1] = ty
     state = habitat_sim.AgentState()
     frame = i + extra_steps
 
-    if i + 1 < len(interpolated):
-        forward = np.array(interpolated[i + 1]) - np.array(pos)
+    if i + 1 < len(path_points):
+        forward = np.array(path_points[i + 1]) - np.array(pos)
         forward[1] = 0  # flatten to straight ahead
-        forward = forward / np.linalg.norm(forward)
-        rotation = utils.quat_from_two_vectors(np.array([0, 0, -1]), forward)
+
+        if all(forward == [0.0, 0.0, 0.0]):
+            continue
+        forward /= np.linalg.norm(forward)
+        print(f"frame {i} - {int(i / len(path_points) * 100)}%", end="\r")
+
+        rotation = utils.quat_from_two_vectors(
+            np.array([0, 0, -1]), forward
+        ).normalized()
 
         if prev_rotation:
             rot_diff = np.quaternion(
@@ -140,6 +160,7 @@ for i, pos in enumerate(interpolated):
 
         # Sharp turn, interpolate further
         if (abs(rot_diff.w) + abs(rot_diff.y)) > rot_threshold:
+            print()
             print("=" * 80)
             print(f"Rotated sharply: {prev_rotation} -> {rotation}")
             print("=" * 80)
@@ -170,6 +191,7 @@ for i, pos in enumerate(interpolated):
         prev_rotation = rotation
         prev_pos = pos
 
+
 with open(f"{output_dir}/rgb-timestamps", "w") as stream:
     stream.writelines(rgb_timestamps)
 
@@ -179,7 +201,6 @@ with open(f"{output_dir}/depth-timestamps", "w") as stream:
 with open(f"{output_dir}/positions", "w") as stream:
     stream.writelines(positions)
 
-print(
-    f"Rendered {len(interpolated) + extra_steps} frames from {start} to {goal}, with seed {seed}"
-)
+print()
+print(f"Rendered {len(path_points) + extra_steps} frames, with seed {seed}")
 sim.close()
